e75202bbe82cfce52eabd06e3e1f8e0b258a0810
HappyMary16 <m.borodin.1999@gmail.com>
2019-04-30 02:53:10 +0300

    add changing style, quote and photos with different moods

---
+++ src/js/ChangeStyle.js
@@ -0,0 +1,20 @@
+function changeStyle() {
+
+    let changeStyleButton = document.getElementById("changeStyleButton");
+
+    let element = document.createElement("link");
+    element.setAttribute("rel", "stylesheet");
+    element.setAttribute("type", "text/css");
+
+    if (changeStyleButton.value === "light") {
+
+        changeStyleButton.value = "dark";
+        element.setAttribute("href", "src/css/darkStyle.css");
+    } else {
+
+        changeStyleButton.value = "light";
+        element.setAttribute("href", "src/css/lightStyle.css");
+    }
+
+    document.getElementsByTagName("head")[0].appendChild(element);
+}
